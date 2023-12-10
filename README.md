ebcbdc5c1b66cfd11fedb644049eac283114f3a8
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-12-10 10:28:41 +0500

    practice streamlit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Room Classification Project
+
+House & Apartaments Classification model
+
+Данная модель оперделяет какая комната в доме или квартире изображена на загруженном в нее изображении
+
+Примеры изображений:
+
+## Экстерьер
+![](/assets/exterior.jpeg)
+
+## Гардеробная
+
+![](/assets/closets.jpg)
+
+## Кухня
+
+![](/assets/kitchen.jpeg)
+
+## Team Member
+- Сидоркин Георгий Владимирович РИМ-130908
+- Рахарди Сандикха РИМ-130908
+- Мухин Виктор Александрович РИМ-130908
+- Шлёгин Лев Русланович РИМ-130908
+## Installation
+```bash
+pip install streamlit
+```
+```bash
+pip install tensorflow
+```
+```bash
+pip install transformers
+```
+
+## Usage
+open terminal and run this command
+
+```bash
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,53 @@
+import io
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+from transformers import pipeline
+
+
+def load_image():
+    uploadedFile = st.file_uploader('Upload image here')
+
+    if uploadedFile is not None:
+        st.write("Filename:", uploadedFile.name)
+        image_data = uploadedFile.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+    
+def preprocess_image(img):
+    img = img.resize((100, 100))
+    x = tf.keras.utils.img_to_array(img)
+    x = tf.keras.applications.efficientnet.preprocess_input(x)
+    return x
+
+def load_model():
+    return pipeline(model="JuanMa360/room-classification")
+
+# Project Title
+st.title("Room Classification Project")
+
+# Team Member
+st.write("""
+         House & Apartaments Classification model
+         #### TEAM MEMBER
+         - Сидоркин Георгий Владимирович РИМ-130908
+         - Рахарди Сандикха РИМ-130908
+         - Мухин Виктор Александрович РИМ-130908
+         -
+         """)
+
+st.write("""#### Our Project""")
+
+# Initial function
+loadedImage = load_image()
+model = load_model()
+
+result = st.button('Submit')
+
+if result:
+    x = preprocess_image(loadedImage)
+    prediction = model.predict(loadedImage)
+    st.write("""#### Output""")
+    st.write(prediction)
